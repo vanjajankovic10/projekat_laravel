60adb50fd35489581d9dbc10bc6fdf91f7b4a91f
Vanja Jankovic <vj20190010@student.fon.bg.ac.rs>
2022-12-20 12:41:42 +0100

    create migracije

---
+++ library-blog/database/migrations/2022_12_20_111429_create_books_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->integer('number of pages');
+            /*$table->foreignId('author_id')->references('id')->on('authors');
+            $table->foreignId('publisher_id')->references('id')->on('publishers');
+            $table->foreignId('genre_id')->references('id')->on('genres');*/
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
